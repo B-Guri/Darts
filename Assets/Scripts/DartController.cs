e60499fa5f5c733ca291980fe93cc55425423daf
Bohdan <hursky73@gmail.com>
2022-11-30 04:33:24 +0200

    update

--- Assets/Scripts/DartController.cs
+++ Assets/Scripts/DartController.cs
@@ -1,12 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DartController : MonoBehaviour
 {
+    [SerializeField] private TextMeshPro text;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
+        if (other.tag == "Target")
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+        if (other.tag == "Ground")
+        {
+            Destroy(this.gameObject);
+        }
+        try
+        {
+            float count = float.Parse(other.name);
+            text.text = (float.Parse(text.text) + count).ToString();
+            GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+        catch
+        {
+            ;
+        }
     }
 }
